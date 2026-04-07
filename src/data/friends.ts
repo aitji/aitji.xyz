480e14e9680d200688a009d6f661628455285b00
aitji <ait.suriya@gmail.com>
2026-04-07 18:20:07 +0700

    update friends list

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -6,8 +6,33 @@ export interface Friend {
 
 export const friends: Friend[] = [
   {
+    name: 'picker',
+    url: 'https://github.com/pickerth-12',
+    desc: 'human',
+  },
+  {
+    name: 'mineners',
+    url: 'https://github.com/minenersGaming',
+    desc: 'math guy',
+  },
+  {
+    name: 'encyptAES',
+    url: 'https://github.com/encyptAES',
+    desc: 'network guy',
+  },
+  {
+    name: 'encyptAES',
+    url: 'https://github.com/encyptAES',
+    desc: 'network guy',
+  },
+  {
     name: 'n1gh7shadez',
     url: 'https://n1gh7shadez.vercel.app',
-    desc: 'does cool stuff',
+    desc: 'gaming guy',
+  },
+  {
+    name: 't4nluxz',
+    url: 'https://github.com/t4nluxz7-bot',
+    desc: 'sweat guy',
   }
 ]
